823f751279a26de8d6e3bdb08d1d9fa48ddc2749
kpwang2 <m13372782171_1@163.com>
2019-08-14 16:56:20 +0800

    wxdc创建项目

---
+++ src/test/java/com/kpwang/sell/LoggerTest.java
@@ -0,0 +1,31 @@
+package com.kpwang.sell;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author kpwang
+ * @create 2019-08-14 14:57
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+//@Data
+public class LoggerTest {
+   // private final Logger logger= LoggerFactory.getLogger(LoggerTest.class);
+    @Test
+    public void test1(){
+        String name="kpwang";
+        String password="123456";
+        log.debug("debug............");
+        log.info("info............");
+        log.info("name:{},password:{}",name,password);
+        log.error("error............");
+    }
+}
